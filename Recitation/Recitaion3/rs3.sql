b924f27e2e66f70124d58be21dbb62697c181d01
Nick Song <nsong@kth.se>
2016-02-10 15:53:38 +0100

    hahah

--- Recitation/Recitaion3/rs3.sql
+++ Recitation/Recitaion3/rs3.sql
@@ -1,1 +1,3 @@
-SELECT firstname, lastname, city  FROM employees;  
+/* SELECT firstname, lastname, EmployeeID ,city  FROM employees ORDER BY city; */
+/* SELECT * FROM OrderDetails; */
+SELECT Orders.OrderID,OrderDetails.OrderID, EmployeeID,SUM(UnitPrice) FROM Orders,OrderDetails GROUP BY EmployeeID/* WHERE Orders.OrderID= OrderDetails.OrderID;*/
